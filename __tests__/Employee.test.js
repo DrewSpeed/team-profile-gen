7a8fa68a9d0999250a676e35b4f816ab2a91b033
Drew Speed <drewjspeed@gmail.com>
2022-02-25 15:33:41 -0600

    testing for Employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require('../lib/Employee');
+
+test('creates an Employee object', () => {
+    const employee = new Employee('Drew', 1138, 'drewjspeed@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    
+});
+
+test('gets employee id', () => {
+    const employee = new Employee('Drew', 1138, 'drewjspeed@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Drew', '1138', 'drewjspeed@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+test('gets employee email', () => {
+    const employee = new Employee ('Drew', '1138', 'drewjspeed@gmail.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+})
